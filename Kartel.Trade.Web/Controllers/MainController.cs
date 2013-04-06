054dde15fe1d1f1dce73cc627ca5e45f155ce6bf
Yuri Korshev <ykorshev@softgears.ru>
2013-04-06 17:59:17 +1000

    Сверстана главная страница проекта Трейд.

---
+++ Kartel.Trade.Web/Controllers/MainController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Kartel.Trade.Web.Controllers
+{
+    /// <summary>
+    /// Основной контроллер системы
+    /// </summary>
+    public class MainController : Controller
+    {
+        /// <summary>
+        /// Отображает главную страницу системы
+        /// </summary>
+        /// <returns></returns>
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+    }
+}
